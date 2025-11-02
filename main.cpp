6d96341733a78ac71d09edeef8165fac29215b6e
tudou <tudou@LAPTOP-IADS47OL>
2025-11-02 22:39:32 +0800

    test

--- main.cpp
+++ main.cpp
@@ -1,74 +1,53 @@
 #include<iostream>
 #include<string>
 #include<algorithm>
+#include<cmath>
+
+//#include<bits/stdc++.h>
 using namespace std;
 
 
-bool is_Answer(int s3,int s4)
-{
-	string s = to_string(s3) + to_string(s4);
-	sort(s.begin(), s.end());
-	for (int i = 0; i < 10; i++)
-	{
-		if (s[i] != '0' + i)
-		{
-			return false;
-		}
-	}
-	return true;
-}
-
-bool is_Answer2(int s3, int s4)//MY
-{
-	 int ans[10] = { 0 };
-	int i = 0;
-	while (s3 >= 1)
-	{
-		ans[i] = s3 % 10;
-		s3 /= 10;
-		i++;
-	}
-	
-	while (s4 >= 1)
-	{
-		ans[i] = s4 % 10;
-		s4 /= 10;
-		i++;
-	}
-
-	sort(ans, ans + 10);
-	for (int i = 0; i < 10; i++)
-	{
-		if (ans[i] != i)
-		{
-			return false;
-		}
-	}
-	return true;
-}
-
-
-
+//int main()
+//{
+//	int i, j;
+//	for (i = 1; i < 30; i++)
+//	{
+//		for (j = 1; j < 30; j++)
+//		{
+//			if ((i * j) == 6 * (i + j) && (abs(i - j) <= 8) && (i != j))
+//			{
+//				/*cout << "i: " << i << " j: " << j << endl;*/
+//				int ans = (i < j ? i : j);
+//				cout << ans << endl;
+//				return 0;
+//			}
+//		}
+//	}
+//
+//	return 0;
+//}
 
 
 int main()
 {
-	for (int i = 10; i < 22; i++)
+	int i;
+	string str;
+	for (i = 1; i <= 2020; i++)
 	{
-		int s3 = i * i * i;
-		int s4 = i * i * i * i;
-		if (s3 >= 1000 && s3 < 10000 && s4 >= 100000 && s4 < 1000000)
-		{
-			if (is_Answer2(s3,s4))
-			{
-
-				cout << "Answer is: " << i << endl;
-
-			}
-
-
-		}
-
+		str.append(to_string(i));
 	}
+	int count = 0;
+	//for (auto s : str)
+	//{
+	//	if (s == '2')
+	//	{
+	//		count++;
+	//	}
+	//}
+
+	count = std::count(str.begin(), str.end(), '2');
+
+	/*cout << str << endl;*/
+	cout << count << endl;
 	return 0;
 }
